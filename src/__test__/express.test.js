03eed93efde2b8b25d8c604cfda94a1d5e233807
Kris3579 <kristianesvelt@hotmail.com>
2018-09-24 21:11:56 -0700

    bug with delete, bug with superagent?

--- src/__test__/express.test.js
+++ src/__test__/express.test.js
@@ -1,22 +1,50 @@
 'use strict';
 
-const superagent = require('superagent');
+process.env.PORT = 3000;
+
 const faker = require('faker');
+const superagent = require('superagent');
 const server = require('../lib/server');
 
-process.env.PORT = 3000;
-const API_URL = `http://localhost:${process.env.PORT}/api/cats`;
+//! Vinicio - setting up the testing port, by HAND
+const API_URL = `http://localhost:${process.env.PORT}/api/notes`;
 
-describe('/api/notes', () => {
+describe('/api/cats', () => {
     beforeAll(server.start);
     afterAll(server.stop);
 
-    test('Should respond with 200 code abd json object if there is a matching id', () => {
+    // test('should respond with 200 status code and a new json note', () => {
+    //     return superagent.post(API_URL)
+    //         .set('Content-Type', 'application/json')
+    //         .send({
+    //             title: 'Gregor',
+    //             content: 'is cute',
+    //         })
+    //         .then((response) => {
+    //             expect(response.status).toEqual(200);
+    //             expect(response.body.content).toEqual('is cute');
+    //             expect(response.body.title).toEqual('Gregor');
+    //             expect(response.body.timestamp).toBeTruthy();
+    //             expect(response.body.id).toBeTruthy();
+    //         });
+    // });
+    // test('should respond with 400 status code if there is no title', () => {
+    //     return superagent.post(API_URL)
+    //         .set('Content-Type', 'application/json')
+    //         .send({
+    //             content: 'is cute',
+    //         })
+    //         .then(Promise.reject)
+    //         .catch((response) => {
+    //             expect(response.status).toEqual(400);
+    //         });
+    // });
+
+    test('should respond with 200 status code and a json note if there is a matching id', () => {
         const originalRequest = {
             title: faker.lorem.words(5),
             content: faker.lorem.words(5),
         };
-
         return superagent.post(API_URL)
             .set('Content-Type', 'application/json')
             .send(originalRequest)
@@ -28,8 +56,7 @@ describe('/api/notes', () => {
                 expect(getResponse.status).toEqual(200);
                 expect(getResponse.body.timestamp).toBeTruthy();
                 expect(getResponse.body.id).toEqual(originalRequest.id);
-                expect(getResponse.body.content).toEqual(originalRequest.content);
                 expect(getResponse.body.title).toEqual(originalRequest.title);
-            })
-    })
-})
+            });
+    });
+});
